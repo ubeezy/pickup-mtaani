3145e1f630ab9e40fba7991b7cb2a1a3711fe11d
jeffubayi <jeffubayi@gmail.com>
2023-06-27 08:42:22 +0300

    add types

---
+++ types/index.tsx
@@ -0,0 +1,18 @@
+export type JokesState = {
+    id: number;
+    Title: string;
+    Author: string;
+    Body: string;
+    Views: number;
+    CreatedAt: Date | number;
+  };
+  
+  export type JokesValues = {
+    id?: number | string | undefined;
+    Title: string | string[] | undefined;
+    Author: string | string[] | undefined;
+    Body?: string | string[] | undefined;
+    Views: string | string[] | undefined;
+    CreatedAt?: Date | string | string[];
+  };
+  
